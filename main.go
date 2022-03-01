cf1963f34768760ebec15e3d65a70cabe7f674d6
Nicholas Ramsay <nick@yousponsor.me>
2022-03-01 15:38:50 +1100

    adding main.go with sample http req

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
